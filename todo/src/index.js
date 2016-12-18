0fcd4cc386a5d01cbd9e2f46485cfff1817d32b3
cirpo <alessandro.cinelli@gmail.com>
2016-12-18 21:49:15 +0000

    with filter

--- todo/src/index.js
+++ todo/src/index.js
@@ -13,7 +13,6 @@ let todoslist = {
   unsubscribe () {
     this.onChange = null
   },
-
   add (todo) {
     this.todos.push(todo)
     this.onChange && this.onChange(this.todos)
@@ -22,6 +21,9 @@ let todoslist = {
   toggleTodo (todo) {
     todo.isCompleted = !todo.isCompleted
     this.onChange && this.onChange(this.todos)
+  },
+  filter (filter) {
+    this.onChange && this.onChange(this.todos, filter)
   }
 }
 
@@ -30,15 +32,15 @@ const withTodos = Component => (class extends React.Component {
     super()
     this.state = {
       todos: [],
-      staus: 'all'
+      filteredTodos: [],
+      filter: 'all'
     }
 
     this.onChange = this.onChange.bind(this)
   }
 
   componentDidMount () {
-    console.log('didmoutn')
-    console.log(todoslist)// eslint-disable-line
+  //  console.log(todoslist)// eslint-disable-line
     todoslist.subscribe(this.onChange) // eslint-disable-line
   }
 
@@ -46,32 +48,58 @@ const withTodos = Component => (class extends React.Component {
     todoslist.unsubscribe() // eslint-disable-line
   }
 
-  onChange (todos) {
-    this.setState({todos})
+  onChange (todos, filter) {
+    console.log('state', this.state)
+    filter = filter || this.state.filter
+    console.log('todos', todos)
+    console.log('filter', filter)
+    let filteredTodos = todos
+
+    if (filter === 'completed') {
+    //  console.log('completed')
+      filteredTodos = todos.filter(todo => todo.isCompleted)
+    }
+
+    if (filter === 'uncompleted') {
+    //  console.log('uncompleted')
+      filteredTodos = todos.filter(todo => !todo.isCompleted)
+    }
+
+    console.log('filteredTodos', filteredTodos)
+    this.setState({todos, filteredTodos, filter})
   }
 
   render () {
-    return <Component todos={this.state.todos} />
+    return <Component todos={this.state.todos} filteredTodos={this.state.filteredTodos} />
   }
 })
 
-let Todos = ({todos}) => ( // eslint-disable-line
+let Todos = ({todos, filteredTodos}) => ( // eslint-disable-line
   <div>
     <Counter todos={todos}/>
+    <Filter />
     <ul>
-      {todos.map((todo, i) => (
+      {filteredTodos.map((todo, i) => (
         <li key={i} onClick={todoslist.toggleTodo.bind(todoslist, todo)}>{todo.title} </li>
       ))}
     </ul>
   </div>
 )
 
+let Filter = ({todos}) => (
+    <div>
+      <div onClick={todoslist.filter.bind(todoslist, 'completed')}>completed  </div>
+      <div onClick={todoslist.filter.bind(todoslist, 'uncompleted')}> uncompleted</div>
+      <div onClick={todoslist.filter.bind(todoslist, 'all')}> all</div>
+    </div>
+)
+
 let Counter = ({todos}) => ( // eslint-disable-line
   <div>
-    <h1>Counter</h1>
-    <h2>total: {todos.length}</h2>
-    <h2>completed: {todos.filter((todo) => todo.isCompleted).length}</h2>
-    <h2>not completed: {todos.filter((todo) => !todo.isCompleted).length}</h2>
+    <h2>Counter</h2>
+    <h4>total: {todos.length}</h4>
+    <h4>completed: {todos.filter((todo) => todo.isCompleted).length}</h4>
+    <h4>not completed: {todos.filter((todo) => !todo.isCompleted).length}</h4>
   </div>
 )
 
