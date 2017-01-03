567d2e9c2fbbf3ae979cec833d27e6da449094da
cirpo <alessandro.cinelli@gmail.com>
2017-01-03 11:21:58 +0000

    small style refactorin

--- src/TodoFilter.js
+++ src/TodoFilter.js
@@ -4,27 +4,30 @@ import Badge from 'material-ui/Badge'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
+const style = {padding: '22px 0px 0px 12px'}
+const badgeStyle = {top: 5, right: 5}
+
 let TodoFilter = ({todos, filter, onFilter}) => (
   <div>
     <Badge
       badgeContent={todos.length}
       secondary={true}
-      badgeStyle={{top: 5, right: 5}}
-      style={{padding: '22px 0px 0px 12px'}}>
+      badgeStyle={badgeStyle}
+      style={style}>
       <FlatButton onClick={() => onFilter(undefined)} label="all" secondary={filter.done === undefined} />
     </Badge>
     <Badge
       badgeContent={todos.filter((todo) => !todo.done).length }
       secondary={true}
-      badgeStyle={{top: 5, right: 5}}
-      style={{padding: '22px 0px 0px 12px'}}>
+      badgeStyle={badgeStyle}
+      style={style}>
       <FlatButton onClick={() => onFilter(false)} label="active" secondary={filter.done === false} />
     </Badge>
     <Badge
       badgeContent={todos.filter((todo) => todo.done).length}
       secondary={true}
-      badgeStyle={{top: 5, right: 5}}
-      style={{padding: '22px 0px 0px 12px'}}>
+      badgeStyle={badgeStyle}
+      style={style}>
       <FlatButton onClick={() => onFilter(true)} label="done" secondary={filter.done === true} />
     </Badge>
   </div>
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,13 +2,14 @@ import React from 'react' // eslint-disable-line no-unused-vars
 import {List, ListItem} from 'material-ui/List'
 import Checkbox from 'material-ui/Checkbox'
 
+const style = { 'borderBottom': 'solid 1px #ccc'}
 
 let TodoList = ({todos, toggleTodo, filter}) => (
 <List>
   {
     todos.map(function (todo, i) { // eslint-disable-line
       if (filter.done === undefined || filter.done === todo.done) {
-        return <ListItem style={{ 'borderBottom': 'solid 1px #ccc'}}
+        return <ListItem style={style}
                 key={i}
                 onClick={() => toggleTodo(todo)}
                 primaryText={todo.title}
